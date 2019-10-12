098b4a21e006df0d1a42afd38e3528b3f2e5f440
dianlujitao <dianlujitao@lineageos.org>
2019-10-12 10:12:37 +0800

    houji: audio: Enable AOSP bluetooth audio HAL v2
    Change-Id: I4d12752ae5fb2829d6612647c8740c84967f5434

--- device.mk
+++ device.mk
@@ -94,6 +94,7 @@ PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/pineapple/microphone_characteristics.xml:$(TARGET_COPY_OUT_VENDOR)/etc/microphone_characteristics.xml
 
 PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
