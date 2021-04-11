b99894cdb44f658721a708ea914bf38f2cdec503
daniml3 <danimoral1001@gmail.com>
2021-04-12 01:02:18 +0200

    sm8650-common: parts: Implement double tap to wake
    * Using Xiaomi's TouchFeature
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: Ie15bea08d8495fff338c71c287f4494e2c59fccd

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.java
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.java
@@ -7,18 +7,26 @@
 package com.xiaomi.settings;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.ContentObserver;
 import android.hardware.display.DisplayManager;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Display;
 import android.view.Display.HdrCapabilities;
+import vendor.xiaomi.hw.touchfeature.ITouchFeature;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final String TAG = "XiaomiParts";
     private static final boolean DEBUG = true;
+    private static final int DOUBLE_TAP_TO_WAKE_MODE = 14;
+
+    private ITouchFeature xiaomiTouchFeatureAidl;
 
     @Override
     public void onReceive(final Context context, Intent intent) {
@@ -33,5 +41,42 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         displayManager.overrideHdrTypes(Display.DEFAULT_DISPLAY,
                 new int[] {HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
                         HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
+
+        ContentObserver observer = new ContentObserver(new Handler()) {
+            @Override
+            public void onChange(boolean selfChange) {
+                updateTapToWakeStatus(context);
+            }
+        };
+
+        context.getContentResolver().registerContentObserver(
+                Settings.Secure.getUriFor(Settings.Secure.DOUBLE_TAP_TO_WAKE), true, observer);
+
+        updateTapToWakeStatus(context);
+    }
+
+    private void updateTapToWakeStatus(Context context) {
+        try {
+            if (xiaomiTouchFeatureAidl == null) {
+                try {
+                    var name = "default";
+                    var fqName =
+                            vendor.xiaomi.hw.touchfeature.ITouchFeature.DESCRIPTOR + "/" + name;
+                    var binder = android.os.Binder.allowBlocking(
+                            android.os.ServiceManager.waitForDeclaredService(fqName));
+                    xiaomiTouchFeatureAidl =
+                            vendor.xiaomi.hw.touchfeature.ITouchFeature.Stub.asInterface(binder);
+                } catch (Exception e) {
+                    Log.e(TAG, "Failed to initialize Touch Feature service", e);
+                }
+            }
+
+            boolean enabled = Settings.Secure.getInt(context.getContentResolver(),
+                                      Settings.Secure.DOUBLE_TAP_TO_WAKE, 0)
+                    == 1;
+            xiaomiTouchFeatureAidl.setTouchMode(0, DOUBLE_TAP_TO_WAKE_MODE, enabled ? 1 : 0);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to update Tap to Wake status", e);
+        }
     }
 }
