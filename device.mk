bb018347e37a0f98b5d6d28d91d0d84e9359d548
Fenglin Wu <quic_fenglinw@quicinc.com>
2022-06-27 23:12:24 +0800

    houji: healthd-ext: Add health HAL AIDL implementation
    Add health HAL AIDL implementation which is a service running for both health HAL and charger mode.
    
    Change-Id: I1f3205d1e34d93ed1739d5fa29c95a8f2b2d2894

--- device.mk
+++ device.mk
@@ -46,6 +46,13 @@ PRODUCT_PACKAGES += \
 TARGET_SCREEN_HEIGHT := 2670
 TARGET_SCREEN_WIDTH := 1200
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery \
+    android.hardware.health@1.0.vendor \
+    android.hardware.health@2.1.vendor
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := true
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
