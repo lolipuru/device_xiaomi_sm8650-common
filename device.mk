9b1497820a0356eb8c7d5f53d8f5aaa11a17750b
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-09-11 16:54:49 +0200

    houji: Switch to custom Xiaomi vibrator HAL
     * this allows us to use Xiaomis custom effects
    
    Change-Id: I9bfd1babaa56b1f6e081835e9593905a9ff8ecfe

--- device.mk
+++ device.mk
@@ -436,6 +436,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.xiaomi \
+    android.hardware.vibrator-V1-ndk_platform.vendor
+
+PRODUCT_COPY_FILES += \
+    hardware/xiaomi/aidl/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
