6f43dc1e069b8cf0904b10af6108eab6c06e632a
Karra Harshitha <quic_kharshit@quicinc.com>
2023-11-09 17:18:41 +0530

    houji: Enable QSPA
    Change-Id: If052ab21ab8fda0cc90b3fca352d1e75f423cb57

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,6 +105,7 @@ BOARD_BOOTCONFIG := \
     androidboot.load_modules_parallel=true \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.vendor.qspa=true \
     androidboot.selinux=permissive
 
 BOARD_KERNEL_IMAGE_NAME := Image
