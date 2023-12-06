a60f81e668539d383a002e19a9123aa5f7655cb7
Arian <arian.kulmer@web.de>
2023-12-06 18:32:23 +0000

    sm8650-common: Import UDFPS handler lib from sm8450-common
    Change-Id: I62fc84a99f4ec31dc1c482cb0c0b3b974681aab3

---
+++ udfps/UdfpsHandler.cpp
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "UdfpsHandler.xiaomi_sm8650"
+
+#include <android-base/logging.h>
+#include <android-base/unique_fd.h>
+
+#include <poll.h>
+#include <fstream>
+#include <thread>
+
+#include "UdfpsHandler.h"
+
+#define COMMAND_NIT 10
+#define PARAM_NIT_FOD 1
+#define PARAM_NIT_NONE 0
+
+#define COMMAND_FOD_PRESS_STATUS 1
+#define PARAM_FOD_PRESSED 1
+#define PARAM_FOD_RELEASED 0
+
+#define FOD_STATUS_PATH "/sys/class/touch/touch_dev/fod_press_status"
+#define FOD_STATUS_OFF 0
+#define FOD_STATUS_ON 1
+
+#define DISP_PARAM_PATH "/sys/devices/virtual/mi_display/disp_feature/disp-DSI-0/disp_param"
+#define DISP_PARAM_LOCAL_HBM_MODE "9"
+#define DISP_PARAM_LOCAL_HBM_OFF "0"
+#define DISP_PARAM_LOCAL_HBM_ON "1"
+
+#define FOD_PRESS_STATUS_PATH "/sys/class/touch/touch_dev/fod_press_status"
+
+namespace {
+
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
+}
+
+static bool readBool(int fd) {
+    char c;
+    int rc;
+
+    rc = lseek(fd, 0, SEEK_SET);
+    if (rc) {
+        LOG(ERROR) << "failed to seek fd, err: " << rc;
+        return false;
+    }
+
+    rc = read(fd, &c, sizeof(char));
+    if (rc != 1) {
+        LOG(ERROR) << "failed to read bool from fd, err: " << rc;
+        return false;
+    }
+
+    return c != '0';
+}
+
+}  // anonymous namespace
+
+class XiaomiSM8650UdfpsHander : public UdfpsHandler {
+  public:
+    void init(fingerprint_device_t* device) {
+        mDevice = device;
+
+        std::thread([this]() {
+            int fd = open(FOD_PRESS_STATUS_PATH, O_RDONLY);
+            if (fd < 0) {
+                LOG(ERROR) << "failed to open fd, err: " << fd;
+                return;
+            }
+
+            struct pollfd fodPressStatusPoll = {
+                    .fd = fd,
+                    .events = POLLERR | POLLPRI,
+                    .revents = 0,
+            };
+
+            while (true) {
+                int rc = poll(&fodPressStatusPoll, 1, -1);
+                if (rc < 0) {
+                    LOG(ERROR) << "failed to poll fd, err: " << rc;
+                    continue;
+                }
+
+                mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_STATUS,
+                                readBool(fd) ? PARAM_FOD_PRESSED : PARAM_FOD_RELEASED);
+            }
+        }).detach();
+    }
+
+    void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
+        LOG(INFO) << __func__;
+        setFingerDown(true);
+    }
+
+    void onFingerUp() {
+        LOG(INFO) << __func__;
+        setFingerDown(false);
+    }
+
+    void onAcquired(int32_t result, int32_t vendorCode) {
+        LOG(INFO) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
+        if (result == FINGERPRINT_ACQUIRED_GOOD) {
+            setFingerDown(false);
+            setFodStatus(FOD_STATUS_OFF);
+        } else if (vendorCode == 21 || vendorCode == 23) {
+            /*
+             * vendorCode = 21 waiting for fingerprint authentication
+             * vendorCode = 23 waiting for fingerprint enroll
+             */
+            setFodStatus(FOD_STATUS_ON);
+        }
+    }
+
+    void cancel() {
+        LOG(INFO) << __func__;
+        setFingerDown(false);
+        setFodStatus(FOD_STATUS_OFF);
+    }
+
+  private:
+    fingerprint_device_t* mDevice;
+
+    void setFodStatus(int value) {
+        set(FOD_STATUS_PATH, value);
+    }
+
+    void setFingerDown(bool pressed) {
+        mDevice->extCmd(mDevice, COMMAND_NIT, pressed ? PARAM_NIT_FOD : PARAM_NIT_NONE);
+
+        set(DISP_PARAM_PATH,
+            std::string(DISP_PARAM_LOCAL_HBM_MODE) + " " +
+                    (pressed ? DISP_PARAM_LOCAL_HBM_ON : DISP_PARAM_LOCAL_HBM_OFF));
+    }
+};
+
+static UdfpsHandler* create() {
+    return new XiaomiSM8650UdfpsHander();
+}
+
+static void destroy(UdfpsHandler* handler) {
+    delete handler;
+}
+
+extern "C" UdfpsHandlerFactory UDFPS_HANDLER_FACTORY = {
+        .create = create,
+        .destroy = destroy,
+};
