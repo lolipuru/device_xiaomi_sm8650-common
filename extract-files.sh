e4459dd7dc6e4c1ba7c609c31e5b173aa9b64092
John Galt <johngaltfirstrun@gmail.com>
2024-09-03 13:46:22 -0400

    [HACK] extract-files: remove vp9 and av1 qti decoder entries
    To force SW decoding. These are broken due to fd handling, which causes
    EGL_BAD_ACCESS and subsequent crashes. This at least allows users to
    watch av1 or vp9 videos (such as on YouTube).

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,8 @@ function blob_fixup() {
             ;;
         vendor/etc/media_codecs_pineapple.xml|vendor/etc/media_codecs_pineapple_vendor.xml)
             sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio)/d" "${2}"
+            sed -i "/vp9.decoder/,/<\/MediaCodec>/d" "${2}"
+            sed -i "/av1.decoder/,/<\/MediaCodec>/d" "${2}"
             ;;
     esac
 }
