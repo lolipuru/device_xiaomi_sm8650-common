4606cd02fe1113dda56e2b95b358c1e061418f55
Adhitya Mohan <me@adhityamohan.in>
2024-10-02 21:57:52 +0200

    sm8650-common: Disable serial console
    * We don't have UART so this is kinda useless

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -97,7 +97,8 @@ BOARD_BOOTCONFIG := \
     androidboot.load_modules_parallel=true \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
-    androidboot.vendor.qspa=true
+    androidboot.vendor.qspa=true \
+    androidboot.console=0
 
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
