31041cd043ecdc8faa66107aae8eeea8086ee69b
lolipuru <banstellazz@gmail.com>
2025-04-13 18:01:36 +0000

    [HACK]: sm8650-common: move mpss symlink back to android.mk for now
    until modem_firmware commit was merged
    
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- Android.mk
+++ Android.mk
@@ -10,4 +10,20 @@ ifneq ($(filter houji shennong aurora ruyi goku manet zorn,$(TARGET_DEVICE)),)
 	include $(call all-makefiles-under,$(LOCAL_PATH))
 	include $(CLEAR_VARS)
 
+RFS_MSM_MPSS_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/mpss
+$(RFS_MSM_MPSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating RFS MSM MPSS folder structure: $@"
+	@rm -rf $@/*
+	@mkdir -p $@/readonly/vendor
+	$(hide) ln -sf /data/vendor/tombstones/rfs/modem $@/ramdumps
+	$(hide) ln -sf /mnt/vendor/persist/rfs/msm/mpss $@/readwrite
+	$(hide) ln -sf /mnt/vendor/persist/rfs/shared $@/shared
+	$(hide) ln -sf /mnt/vendor/persist/hlos_rfs/shared $@/hlos
+	$(hide) ln -sf /data/vendor/mbnconfig $@/readonly/mbnconfig
+	$(hide) ln -sf /vendor/modem_firmware $@/readonly/firmware
+	$(hide) ln -sf /vendor/firmware $@/readonly/vendor/firmware
+
+ALL_DEFAULT_INSTALLED_MODULES += \
+	$(RFS_MSM_MPSS_SYMLINKS)
+
 endif
