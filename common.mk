d8ac14f1bae8c8dd32fcdde9c55dc47390883837
lolipuru <banstellazz@gmail.com>
2025-05-29 18:03:48 +0700

    sm8650-common: switch back to qti health services
    https://github.com/MiCode/Xiaomi_Kernel_OpenSource/blob/shennong-u-oss/drivers/power/supply/qti_battery_charger.c#L2080-L2089
    
    Change-Id: Ibebd42db07b8cb7e2f71ff22065c4489bea3ba19
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- common.mk
+++ common.mk
@@ -180,8 +180,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.xiaomi \
-    android.hardware.health-service.xiaomi_recovery
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 # IPACM
 PRODUCT_PACKAGES += \
