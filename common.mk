f60aa9c7a150f0f7e40e1dd62622b307346d075e
lolipuru <banstellazz@gmail.com>
2025-08-24 20:12:12 +0000

    sm8650-common: re-import audio config
    - from stock OS2.0.200.13.VNCCNXM
    - also few config import from AUDIO.LA.9.0.r4-02400-lanai.0 as well
    
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- common.mk
+++ common.mk
@@ -107,7 +107,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_pineapple_qssi/audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/bluetooth_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_hearing_aid_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
