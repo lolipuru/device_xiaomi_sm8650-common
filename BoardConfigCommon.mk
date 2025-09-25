71abea2a907825c5eb24b95ee35c5ca54fa1c79b
lolipuru <banstellazz@gmail.com>
2025-09-26 01:11:51 +0700

    sm8650-common: switch to ext4
    now it's finally bootable with fixed fstab
    
    Change-Id: I2dad91a750f51bdf0d3045d370142bff1c6c4a26
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -119,7 +119,7 @@ BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := odm product system system_dlkm sy
 BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 8317304832
 
 BOARD_PARTITION_LIST := $(call to-upper, $(BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST))
-$(foreach p, $(BOARD_PARTITION_LIST), $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := erofs))
+$(foreach p, $(BOARD_PARTITION_LIST), $(eval BOARD_$(p)IMAGE_FILE_SYSTEM_TYPE := ext4))
 $(foreach p, $(BOARD_PARTITION_LIST), $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
 # Platform
