39c113ae1f76357e2bfe686798e522ed0fdf15f0
lolipuru <banstellazz@gmail.com>
2025-12-07 11:27:05 +0000

    sm8650-common: import hotword permissions
    Change-Id: I166c7ce2381e8efed075bdb855e747c6d1063039
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- common.mk
+++ common.mk
@@ -193,6 +193,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
+# Hotword Enrollement
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
