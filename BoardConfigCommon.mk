702595cfa23e915830c1579543dad6cc3488eda4
LuK1337 <priv.luk@gmail.com>
2025-12-12 11:40:05 +0100

    sm8650-common: Remove vendor/lineage device framework matrix inclusion
    Change-Id: I75776392ffb64c7adcd5cc1fe024dac0c35f912b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,8 +161,7 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/configs/vintf/compatibility_matrix.device.xml \
     $(COMMON_PATH)/configs/vintf/compatibility_matrix.xiaomi.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_MANIFEST_FILE += \
     $(COMMON_PATH)/configs/vintf/framework_manifest.xml
