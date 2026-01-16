7ea3c1dd1f2479d1d418e617f244a3354bbc5093
lolipuru <banstellazz@gmail.com>
2026-01-16 17:42:24 +0000

    sm8650-common: rename CarrierConfig to CarrierConfigXiaomi
    avoid conflict with existing CarrierConfig
    
    Change-Id: If4bb1f164172e23d1493204f5405c928f0bad147
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- common.mk
+++ common.mk
@@ -261,7 +261,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
-    CarrierConfig \
+    CarrierConfigXiaomi \
     FrameworksResXiaomi \
     NcmTetheringOverlay \
     SettingsResXiaomi \
