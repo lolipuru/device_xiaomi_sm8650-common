c68f036ad0dd2dad6b42a634c93dc81f5e1d7626
lolipuru <banstellazz@gmail.com>
2026-01-23 12:52:45 +0000

    test
    Change-Id: I8eb3d6d1d0ee59c7c76016626cdd59eb48f4e229

--- extract-files.py
+++ extract-files.py
@@ -46,19 +46,8 @@ lib_fixups: lib_fixups_user_type = {
         'libmilut',
         'libmips',
         'libmisr',
-        'libagmclient',
         'libagmmixer',
-        'libar-acdb',
-        'libats',
-        'liblx-osal',
-        'liblx-ar_util',
-        'libar-gpr',
-        'libar-gsl',
-        'libpalclient',
-        'libwpa_client',
         'vendor.qti.hardware.display.composer3-V1-ndk',
-        'vendor.qti.hardware.AGMIPC@1.0-impl',
-        'vendor.qti.hardware.pal@1.0-impl',
     ): lib_fixup_remove,
 }
 
@@ -87,8 +76,12 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/bin/cnd',
     ): blob_fixup()
         .add_needed('libbinder_shim.so'),
-    ('vendor/lib64/hw/audio.primary.pineapple.so', 'vendor/lib64/libaudioroute_ext.so'): blob_fixup()
-        .add_needed('libaudioroute-v34.so'),
+    (
+        'vendor/lib64/hw/audio.primary.pineapple.so', 
+        'vendor/lib64/libaudioroute_ext.so',
+        'vendor/lib64/libar-pal.so',
+    ): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
     (
         'vendor/lib64/libdlbdsservice.so',
         'vendor/lib64/libdlbpreg.so',
