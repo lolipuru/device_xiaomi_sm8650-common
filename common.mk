363b06962261cdeddadc95d8bbd61efe8355ddec
lolipuru <banstellazz@gmail.com>
2026-06-23 15:07:14 +0000

    sm8650-common: update soong config for fingerprint
    follow hardware/xaiomi
    
    Change-Id: Id74c1e3df234d9b8f3be87159746c8756497aa2b
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- common.mk
+++ common.mk
@@ -161,7 +161,7 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
-$(call soong_config_set,XIAOMI_BIOMETRICS_FINGERPRINT,USE_NEW_IMPL,true)
+$(call soong_config_set,XIAOMI_BIOMETRICS_FINGERPRINT,IMPL_VER,V2)
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi \
     libudfpshandler
